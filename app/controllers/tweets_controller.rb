773c6e9690f91063d4f503ee1c0a802fc414197f
Nicole Dow <nikki.dow71@gmail.com>
2019-09-04 14:31:34 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -55,34 +55,34 @@ end
  end
  
  
-# patch '/tweets/:id' do
-#     if logged_in?
-#       if params[:content] == ""
-#         redirect to "/tweets/#{params[:id]}/edit"
-#       else
-#         @tweet = Tweet.find_by_id(params[:id])
-#         if @tweet && @tweet.user == current_user
-#           if @tweet.update(content: params[:content])
-#             redirect to "/tweets/#{@tweet.id}"
-#           else
-#             redirect to "/tweets/#{@tweet.id}/edit"
-#           end
-#         else
-#           redirect to '/tweets'
-#         end
-#       end
-#     else
-#       redirect to '/login'
-#     end
-#   end
- 
- 
  patch '/tweets/:id' do
-   #binding.pry
-   @tweet = Tweet.find_by_id(params["id"])
-   @tweet.update(content: params["content"])
-   redirect to '/tweets'
- end
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/#{params[:id]}/edit"
+      else
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && @tweet.user == current_user
+          if @tweet.update(content: params[:content])
+            redirect to "/tweets/#{@tweet.id}"
+          else
+            redirect to "/tweets/#{@tweet.id}/edit"
+          end
+        else
+          redirect to '/tweets'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+ 
+ 
+# patch '/tweets/:id' do
+#   #binding.pry
+#   @tweet = Tweet.find_by_id(params["id"])
+#   @tweet.update(content: params["content"])
+#   redirect to '/tweets'
+# end
  
  
  delete '/tweets/:id/delete' do
