05e96748a807bdadf2a168a06400e0a9602ab330
Nicole Dow <nikki.dow71@gmail.com>
2019-09-04 13:32:29 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,7 +22,7 @@ class TweetsController < ApplicationController
  
  
  post '/tweets' do
-  binding.pry 
+  if params["content"] != "" 
   @new_tweet = current_user.tweets.build(content: params["content"])
   @new_tweet.save
   redirect to '/tweets'
