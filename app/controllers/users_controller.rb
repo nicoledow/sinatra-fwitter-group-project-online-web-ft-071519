1ceed2677eac95dc676f919f30151befad6155cd
Nicole Dow <nikki.dow71@gmail.com>
2019-09-04 13:22:17 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   post '/login' do
     binding.pry
     @user = User.find_by(username: params["user"]["username"])
-    if @user && @user.authenticate(params["password"])
+    if @user && @user.authenticate(params["user"]["password"])
       session[:id] = @user.id
       redirect to '/tweets'
     else
